c8ef4e7900ef29ebe2b3d17c4466b03ecea3f73d
morisum <moris@r.sannet.ne.jp>
2023-03-26 08:38:30 +0900

    Initial Commit

---
+++ Janken1/Janken1App.swift
@@ -0,0 +1,17 @@
+//
+//  Janken1App.swift
+//  Janken1
+//
+//  Created by MsMacM on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Janken1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
