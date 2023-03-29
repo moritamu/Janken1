ad86da79b44cb2d365817410aea38b8dae243fc9
morisum <moris@r.sannet.ne.jp>
2023-03-29 17:52:03 +0900

    scaletoFit

--- Janken1/ContentView.swift
+++ Janken1/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var num1: Int = 2
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200,height: 200)
+                .foregroundColor(.orange)
             Text("Hello, world!")
+            Text(String(num1))
+                .font(.largeTitle)
+                .foregroundColor(.orange)
+                .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: 50)
+                .border(.blue)
+            Button("ボタンを押す") {
+                num1 = Int.random(in: 1...100)
+            }
+            .font(.largeTitle)
+            .foregroundColor(.red)
         }
         .padding()
     }
